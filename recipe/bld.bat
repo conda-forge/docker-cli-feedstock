7677ddf8a3310a006508a8b6d73c01422ac42721
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-17 22:05:22 -0800

    fix build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
 go mod init "github.com/docker/cli" || goto :error
+go get google.golang.org/genproto@latest  || goto :error
 go mod edit -replace "github.com/imdario/mergo@v1.0.0=github.com/imdario/mergo@v0.3.16" || goto :error
 go mod tidy -e || goto :error
-go mod vendor -e || goto :error
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\docker.exe -ldflags="-s" .\cmd\docker || goto :error
+go build -mod=mod -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\docker.exe -ldflags="-s" .\cmd\docker || goto :error
 
 go-licenses save .\cmd\docker --save_path=license-files || goto :error
 
